0cdd54dc9f671e22a7c16fb3c0211bceb378f0a8
EdilvoLima <edilvolima@gmail.com>
2018-10-21 16:22:17 -0300

    1052

---
+++ Provas/P1/1052.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+	
+	int m;
+
+	printf("mes: ");
+	scanf("%d", &m);
+
+	switch (m){
+		case 1:
+		printf("janeiro\n");
+		break;
+
+		case 2:
+		printf("fevereiro\n");
+		break;
+
+		case 3:
+		printf("marco\n");
+		break;
+
+		case 4:
+		printf("Abril\n");
+		break;
+
+		case 5:
+		printf("maio\n");
+		break;
+
+		case 6:
+		printf("junho\n");
+		break;
+
+		case 7:
+		printf("julho\n");
+		break;
+
+		case 8:
+		printf("agosto\n");
+		break;
+
+		case 9:
+		printf("setembro\n");
+		break;
+
+		case 10:
+		printf("outubro\n");
+		break;
+
+		case 11:
+		printf("novembro\n");
+		break;
+
+		case 12:
+		printf("dezembro\n");
+		break;
+
+	}
+	
+
+	return 0;
+}
